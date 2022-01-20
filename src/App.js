25d566d042ac8c2d65d36c8bb7a26204febb3128
StephensGit <stephenmoran@live.ie>
2022-01-20 23:13:35 +0000

    Prettier installed on project

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 import { Nav } from './components/Nav';
 
 export function App() {
+  console.log('Hello');
   return (
     <div>
-      <h1>Hello, {new Date().toString()}</h1>
+      <h1>
+        Hello, {new Date().toString()}
+      </h1>
       <Nav />
     </div>
   );
